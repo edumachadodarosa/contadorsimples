e6020556192af82dd852a3016dd49bddced353f7
edumachadodarosa <edumachadodarosa@gmail.com>
2024-01-12 09:12:05 -0300

    Contador Simples

---
+++ script.js
@@ -0,0 +1,34 @@
+function contar () {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossivel contar'
+    } else {
+         res.innerHTML = 'Contando...'
+         var i = Number(ini.value)
+         var f = Number(fim.value)
+         var p = Number(passo.value)
+         if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+         }
+        if ( i < f ) {
+            //contagem crescente
+            for (var c = i; c <= f; c+= p){
+                res.innerHTML += `${c} \u{1f449}`
+             }
+             res.innerHTML += `\u{1f3c1}`
+        } else {
+            //contagem regressiva
+            for ( var c = i; c >= f; c -= p) {
+                res. innerHTML += `${c} \u{1F449}`
+            }
+
+        }
+       res.innerHTML += `\u{1f3c1}`
+    }
+}
